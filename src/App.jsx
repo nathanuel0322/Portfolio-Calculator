72bcf8dc104cc26596fc331ead48f3857ca11db7
Camilo <camilochafloque@gmail.com>
2023-05-22 14:04:42 -0500

    Fixed small bug in GET

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,6 @@ import "./App.css";
 import { getHistoricalDataBySymbol, getAllSymbols } from "./utils/WTDApi";
 
 function App() {
-  const obj = getHistoricalDataBySymbol("2020-02-20", "GOOG", 32500 * 0.2);
-  console.log(obj);
-
-  console.log(getAllSymbols());
-
   return <div>Portfolio Calculator</div>;
 }
 
