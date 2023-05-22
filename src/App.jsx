b5121585ee1adc528bf70f0555984b253aa4faec
prnvjn <prnvjn@gmail.com>
2023-05-22 15:20:30 -0500

    Creating a branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import { PortfolioForm } from "./components/PortfolioForm";
 
 function App() {
-  return <div>Portfolio Calculator</div>;
+  return <div >Portfolio Calculator
+  <div className="container mx-auto">
+<PortfolioForm/>
+  </div>
+
+  
+  </div>;
 }
 
 export default App;
---
+++ src/components/PortfolioForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const PortfolioForm = () => {
+  return (
+    <div className='bg-slate-500'>PortfolioForm
+    
+    <form>
+<input type="number" name="" id="" />
+
+
+    </form>
+    </div>
+  )
+}
