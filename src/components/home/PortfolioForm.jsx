a55e68f1167fa77fa5d31b38bc5b845f82cf9a48
prnvjn <prnvjn@gmail.com>
2023-05-26 02:15:02 -0500

    changed UI

--- src/components/home/PortfolioForm.jsx
+++ src/components/home/PortfolioForm.jsx
@@ -73,21 +73,14 @@ export const PortfolioForm = ({ setFormData }) => {
   return (
     <form id='portfolioform' className='flex items-center relative mx-auto flex-col w-max-2 justify-start rounded-lg p-2' onSubmit={(e) => submitfunc(e)}>
       <input className='my-3 text-center' type="number" name="" id="" placeholder='Enter your starting balance' value={dataobj.balance} onChange={(event) => setDataobj({...dataobj, balance: parseInt(event.target.value)})} />
-      <div className=' text-md flex justify-between w-80 items-center p-2'>
-        <span id="addstocks">Add stocks</span>
-        <span onClick={addStockButton} id='plusbutton'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-9 h-9">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-          </svg>
-        </span>
-      </div>
+    
       <ul>
         {selectedOptions.map((stock)=> 
           <li className='flex justify-between font-medium  items-center max-h-96 w-80' key={stock}> 
             <input type="checkbox" className="w-7 h-7" checked={selectedOptions.includes(stock)} onChange={() => handleOptionToggle(stock)} 
               htmlFor={stock}
             /> 
-            <label htmlFor={stock} className=' '>{stock}</label>  
+            <label htmlFor={stock} >{stock}</label>  
             <input type="number" className="percents w-10 h-7 text-base" htmlFor={stock} max='100' min='0' placeholder='0' onChange={(event) => {
               const newAllocation = dataobj.allocation.filter((item) => item.symbol !== stock);
               setDataobj({...dataobj, allocation: [...newAllocation, {symbol: stock, weight: parseFloat(event.target.value)}]});
@@ -95,13 +88,13 @@ export const PortfolioForm = ({ setFormData }) => {
           </li>
         )}
       </ul>
-      {!addStock && <input type="text" className='mb-3' value={inputValue} onChange={handleTicker} />}  
-      <ul className="max-h-96 w-80	overflow-y-scroll  bg-purple-400 rounded-sm ">
+      {!addStock && <input type="text" className='m-3' value={inputValue} onChange={handleTicker} />}  
+      <ul className="max-h-96 w-80	overflow-y-scroll drop-shadow-md rounded-m p-2">
         {/* add a check box */}
         {inputValue.length !== 0 &&
           suggestedValues.map((value, index) => 
-            <li className='text-purple-600 bg-slate-200 text-m m-1 text-left p-1 ' key={index}>
-              <label className='flex justify-between font-medium items-center'>
+            <li className=' bg-blue-50 text-m m-2 text-left p-1 rounded-md ' key={index} >
+              <label className='flex justify-between font-medium items-center gradientText'>
                 {value.symbol}
                 <input type="checkbox" className=' w-7 h-7' checked={selectedOptions.includes(value.symbol)}
                   onChange={() => {
@@ -114,6 +107,14 @@ export const PortfolioForm = ({ setFormData }) => {
             </li>
         )}
       </ul>
+      <div className=' text-md flex justify-between w-80 items-center p-2'>
+        <span id="addstocks" >Add stocks</span>
+        <span onClick={addStockButton} id='plusbutton'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-9 h-9">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+          </svg>
+        </span>
+      </div>
       <ReactDatePicker
         className='w-10'
         selected={dataobj.start}
