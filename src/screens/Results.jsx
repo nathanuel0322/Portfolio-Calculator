8d360595e52ac459836a29bdbade381d7f9e771b
Nathanuel Dixon <nathan0322@hotmail.com>
2023-05-26 01:36:33 -0400

    praise God man

--- src/screens/Results.jsx
+++ src/screens/Results.jsx
@@ -11,7 +11,7 @@ export default function Results() {
   const location = useLocation();
   console.log("location.state: ", location.state)
   const givendata = location.state.filteredRange;
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
   console.log("givendata: ", givendata)
 
   useEffect(() => {
@@ -20,14 +20,25 @@ export default function Results() {
       if (Object.hasOwnProperty.call(givendata, stockname)) {
         const stockData = givendata[stockname].data.map((entry) => {
           const splitDate = entry.date.split("-");
+          // if the date already exists in the data array, add the new stock's value to the existing date
+          // else, add a new date to the data array
+          const date = `${splitDate[1]}-${splitDate[2]}-${splitDate[0]}`;
+          const existingIndex = data.findIndex((obj) => obj.date === date);
+
+          if (existingIndex !== -1) {
+            data[existingIndex][stockname] = parseFloat((Math.round(givendata[stockname].sharesondayone * entry.close * 100) / 100).toFixed(2));
+            return;
+          }
           return {
             // convert from YYYY-MM-DD to MM-DD-YYYY
             date: `${splitDate[1]}-${splitDate[2]}-${splitDate[0]}`,
             // round to 2 decimal places and include extra 0s if needed to represent cents
-            [stockname]: (Math.round(givendata[stockname].sharesondayone * entry.close * 100) / 100).toFixed(2),
+            [stockname]: parseFloat((Math.round(givendata[stockname].sharesondayone * entry.close * 100) / 100).toFixed(2)),
           };
         });
-        data.push(...stockData);
+        // filter stockdata since it may contain undefined values
+        // data.push(...stockData);
+        data.push(...stockData.filter((val) => val !== undefined));
       }
     }
     console.log("data: ", data)
@@ -46,7 +57,7 @@ export default function Results() {
         </button>
       </div>
       <h1>Results</h1>
-      {data.length > 0 && (
+      {Object.keys(data).length > 0 && (
         <ResponsiveContainer width="100%" height="100%">
           <LineChart
             width={500}
@@ -65,16 +76,16 @@ export default function Results() {
             <Legend height={36} />
               {/* Render Line components for each stock dynamically */}
               {Object.keys(data[0])
-                .filter((key) => key !== 'date') // Exclude the 'name' property from rendering as a line
-                .map((key, index) => (
+                .filter((key) => key !== 'date')
+                .map((entry, index) => (
                   <Line
                     key={index}
                     type="monotone"
-                    dataKey={key}
-                    stroke={`hsl(${Math.random() * 360}, 50%, 60%)`} // Generate a random stroke color
+                    dataKey={entry}
+                    stroke={`hsl(${Math.random() * 360}, 50%, 60%)`}
                     activeDot={{ r: 8 }}
                   />
-                ))}
+              ))}
           </LineChart>
         </ResponsiveContainer>
       )}
