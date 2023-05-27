ba59fb4835c99c8072612b3a1d8b1a48dac254b3
Nathanuel Dixon <nathan0322@hotmail.com>
2023-05-26 20:00:33 -0400

    fixed results profits

--- src/components/results/ProfitsBoard.jsx
+++ src/components/results/ProfitsBoard.jsx
@@ -4,6 +4,7 @@ const ProfitsBoard = ({ data }) => {
   return (
     <div className="flex flex-col items-center justify-center bg-blue-50 shadow-md rounded-lg py-4 gap-y-4">
       {Object.entries(data).map(([key, value]) => {
+        const tempnum = (Math.round(value.sharesondayone * value.data.slice(-1)[0].close * 100) / 100).toFixed(2);
         return (
           <div key={key}>
             <p className="text-4xl mb-2">Allocation: {key}</p>
@@ -14,12 +15,7 @@ const ProfitsBoard = ({ data }) => {
               Number of shares: {value.sharesondayone.toFixed(2)}
             </p>
             <p className="text-base">
-              Today's Profit:{" "}
-              {`$${(
-                Math.round(
-                  value.sharesondayone * value.data.slice(-1)[0].close * 100
-                ) / 100
-              ).toFixed(2)}`}
+              Today's Profit: <span style={{color: parseFloat(tempnum) >= 0 ? 'green' : 'red'}}>${tempnum}</span>
             </p>
           </div>
         );
