7f3f292796695287063763a12eaae2831a56c93d
Nathanuel Dixon <nathan0322@hotmail.com>
2023-06-26 21:48:04 -0400

    still

--- src/firebase.js
+++ src/firebase.js
@@ -11,12 +11,18 @@ console.log("messaging sender id:", process.env.NODE_ENV === 'production' ? proc
 console.log("app id:", process.env.NODE_ENV === 'production' ? process.env.VITE_APP_ID : import.meta.env.VITE_APP_ID)
 
 const firebaseConfig = {
-    apiKey: process.env.NODE_ENV === 'production' ? process.env.VITE_API_KEY : import.meta.env.VITE_API_KEY,
-    authDomain: process.env.NODE_ENV === 'production' ? process.env.VITE_AUTH_DOMAIN : import.meta.env.VITE_AUTH_DOMAIN,
-    projectId: process.env.NODE_ENV === 'production' ? process.env.VITE_PROJECT_ID : import.meta.env.VITE_PROJECT_ID,
-    storageBucket: process.env.NODE_ENV === 'production' ? process.env.VITE_STORAGE_BUCKET : import.meta.env.VITE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NODE_ENV === 'production' ? process.env.VITE_MESSAGING_SENDER_ID : import.meta.env.VITE_MESSAGING_SENDER_ID,
-    appId: process.env.NODE_ENV === 'production' ? process.env.VITE_APP_ID : import.meta.env.VITE_APP_ID
+    // apiKey: process.env.NODE_ENV === 'production' ? process.env.VITE_API_KEY : import.meta.env.VITE_API_KEY,
+    apiKey: import.meta.env.VITE_API_KEY,
+    // authDomain: process.env.NODE_ENV === 'production' ? process.env.VITE_AUTH_DOMAIN : import.meta.env.VITE_AUTH_DOMAIN,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    // projectId: process.env.NODE_ENV === 'production' ? process.env.VITE_PROJECT_ID : import.meta.env.VITE_PROJECT_ID,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    // storageBucket: process.env.NODE_ENV === 'production' ? process.env.VITE_STORAGE_BUCKET : import.meta.env.VITE_STORAGE_BUCKET,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    // messagingSenderId: process.env.NODE_ENV === 'production' ? process.env.VITE_MESSAGING_SENDER_ID : import.meta.env.VITE_MESSAGING_SENDER_ID,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    // appId: process.env.NODE_ENV === 'production' ? process.env.VITE_APP_ID : import.meta.env.VITE_APP_ID
+    appId: import.meta.env.VITE_APP_ID
 };
 
 export const Firebase = initializeApp(firebaseConfig);
